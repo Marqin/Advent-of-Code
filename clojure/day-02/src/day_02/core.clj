6e29027bb20477ce5414df49d28431c29dcd0e01
Hubert Jarosz <marqin.pl@gmail.com>
2016-07-23 17:11:02 +0200

    [Clojure] Day 02 part 2

--- clojure/day-02/src/day_02/core.clj
+++ clojure/day-02/src/day_02/core.clj
@@ -3,18 +3,26 @@
 
 (require 'clojure.string)
 (import 'java.lang.Integer)
+(import 'java.io.BufferedReader)
 
-(defn sides [h l w]
+(defn get-facets [[h l w]]
   [(* l w) (* h w) (* l h)])
 
-(defn paper [[h l w]]
-  (let [sizes (sides h l w)]
+(defn paper [dimensions]
+  (let [facets (get-facets dimensions)]
     (+
-      (reduce + (map #(* 2 %) sizes))
-      (apply min sizes))))
+      (reduce + (map #(* 2 %) facets))
+      (apply min facets))))
+
+(defn ribbon [dimensions]
+  (+
+    (* 2 (reduce + (take 2(sort dimensions))))
+    (reduce * dimensions)))
 
 (defn parse-line [line]
-  (paper (map #(Integer/parseInt %) (clojure.string/split line #"x" ))))
+  (map #(Integer/parseInt %) (clojure.string/split line #"x" )))
 
 (defn -main []
-  (println (reduce + (map parse-line (line-seq (java.io.BufferedReader. *in*))))))
+  (let [boxes (map parse-line (line-seq (BufferedReader. *in*)))]
+      (println (reduce + (map paper boxes)))
+      (println (reduce + (map ribbon boxes)))))
